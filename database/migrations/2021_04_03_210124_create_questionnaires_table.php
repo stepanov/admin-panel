1e0d5c2a7983ab4a656b44226882842b2d8a72f0
Michael Stepanov <michael@stepanoff.org>
2021-04-04 10:49:19 +0300

    Adjusted migrations

--- database/migrations/2021_04_03_210124_create_questionnaires_table.php
+++ database/migrations/2021_04_03_210124_create_questionnaires_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateQuestionnairesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,13 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('questionnaires', function (Blueprint $table) {
             $table->uuid('id');
             $table->string('name');
+            $table->date('date_of_birth');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
-            $table->enum('type', ['user', 'manager'])->default('user');
+            $table->string('phone')->unique();
+            $table->enum('status', ['new', 'accepted', 'rejected', 'deleted']);
             $table->timestamps();
         });
     }
@@ -32,6 +31,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('questionnaires');
     }
 }
