bd7f558c2aca249dd88f3a8fed35df36608dc9d9
Michael Stepanov <michael@stepanoff.org>
2021-04-04 21:40:57 +0300

    Added API routes

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::prefix('v1')->group(function () {
+    Route::post('/users', 'UserController@authentificate');
+    Route::put('/users', 'UserController@initializeGA');
+    Route::get('/questionnaires', 'QuestionnaireController@index');
+    Router::put('/questionnaires', 'QuestionnaireController@process');
+    Route::delete('/questionnaires', 'QuestionnaireController@delete');
+    Route::get('/logs', 'LogConteller@index');
+});
+
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
