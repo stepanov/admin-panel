2d8b53c03f898017d2d92ceca863d397c66935cd
Michael Stepanov <michael@stepanoff.org>
2021-04-04 13:12:48 +0300

    Added relationships for models

--- app/Models/Questionnaire.php
+++ app/Models/Questionnaire.php
@@ -8,4 +8,20 @@ use Illuminate\Database\Eloquent\Model;
 class Questionnaire extends Model
 {
     use HasFactory;
+
+    /**
+     * Get user that owns the questionnaire
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    /**
+     * Get uploads associated with this user
+     */
+    public function uploads()
+    {
+        return $this->hasMany(Upload::class);
+    }
 }
